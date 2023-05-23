347d69e89bdef21f1dbb39c2c0eefed1bf1b0f62
Shandukani02 <Shandukani700@gmail.com>
2023-05-23 11:07:43 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,25 +1,18 @@
 #include "main.h"
 /**
- * print_last_digit - prints the last digit of a number
+ * _abs - computes the absolute value of an integrer
  *
- * @n: the int to extract the last digit from
+ * @n: the int to check
  *
- * Return: value of the last digit
+ * Return: the absolute value of int
  *
  */
-int print_last_digit(int n)
+int _abs(int n)
 {
-	int a;
-
-	if (n < 0)
-		n = -n;
-
-	a = n % 10;
-
-	if (a < 0)
-		a = -a;
-
-	_putchar(a + '0');
-
-	return (a);
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
 }
+
